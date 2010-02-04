af91fe0c7dd5aa8eb4409b4b2ef4a5bd459b0a58
Brandon Arbini <brandon@sevenwire.com>
2010-02-03 23:43:55 -0800

    Allowing a custom host (specifically for Google Apps).

--- lib/xmpp4r-simple.rb
+++ lib/xmpp4r-simple.rb
@@ -83,9 +83,10 @@ module Jabber
     # passed in as the status_message argument.
     #
     # jabber = Jabber::Simple.new("me@example.com", "password", "Chat with me - Please!")
-    def initialize(jid, password, status = nil, status_message = "Available")
+    def initialize(jid, password, host = nil, status = nil, status_message = "Available")
       @jid = jid
       @password = password
+      @host = host
       @disconnected = false
       status(status, status_message)
       start_deferred_delivery_thread
@@ -391,7 +392,7 @@ module Jabber
       # Connect
       jid = JID.new(@jid)
       my_client = Client.new(@jid)
-      my_client.connect
+      my_client.connect(@host)
       my_client.auth(@password)
       self.client = my_client
 
